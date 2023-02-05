2063c7cb3a21cebc967083b6f63485c2c329fa3a
Manish <manish@dowellresearch.in>
2023-02-05 17:42:28 +0530

    Completed

--- DOM-Project-06/README.md
+++ DOM-Project-06/README.md
@@ -10,6 +10,14 @@ companyLogo.setAttribute('src', './assets/ineuron-logo.png');
 ```javascript
 var priceSpan = document.getElementsByClassName('app_price')[0].children[0];
 priceSpan.innerHTML='$10'
+
+// social media
+var footer_social = document.querySelector(".footer_social");
+
+var footer_social_ico = document.createElement("div");
+footer_social.appendChild(footer_social_ico);
+footer_social_ico.setAttribute('class','footer_social_ico');
+footer_social_ico.innerHTML='<i class="fa-brands fa-linkedin"></i>';
 ```
 
 
--- DOM-Project-06/script.js
+++ DOM-Project-06/script.js
@@ -3,7 +3,16 @@
 var companyLogo = document.getElementsByClassName('logo')[0];
 companyLogo.setAttribute('src', './assets/ineuron-logo.png');
 
-// Question 2 : change the price
+// Question 2 : change the price and social media
 
 var priceSpan = document.getElementsByClassName('app_price')[0].children[0];
 priceSpan.innerHTML='$10'
+
+
+// social media
+var footer_social = document.querySelector(".footer_social");
+
+var footer_social_ico = document.createElement("div");
+footer_social.appendChild(footer_social_ico);
+footer_social_ico.setAttribute('class','footer_social_ico');
+footer_social_ico.innerHTML='<i class="fa-brands fa-linkedin"></i>';
